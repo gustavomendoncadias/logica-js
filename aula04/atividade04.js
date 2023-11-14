45ddf83210956d4df26f58b1bfda33ad4b76e732
gustavomendoncadias <142364569+gustavomendoncadias@users.noreply.github.com>
2023-11-14 00:44:58 +0000

    frutas

---
+++ aula04/atividade04.js
@@ -0,0 +1,9 @@
+let frutas = ["🍇","🍓","🍊","🥑"]
+frutas.push("🍐")
+for (let i = 0; i < frutas.length; i++) 
+    console.log(frutas[i]);
+
+//     console.log(frutas[0])
+// console.log(frutas[1])
+// console.log(frutas[2])
+// console.log(frutas[3])
